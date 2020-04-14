23ecef642fbb7a5065e434e6d7a0dbe79eb98b17
svj <sojoe02@gmail.com>
2020-04-14 15:16:10 +0200

    fixed spotify_control script album art handling (spotify changed the API)

--- media/spotify_dbus.py
+++ media/spotify_dbus.py
@@ -2,12 +2,13 @@
 
 import time
 import requests
+from requests import HTTPError
 import dbus
 import argparse
 import sys
 import imghdr
 import logging
-
+import json
 from pathlib import Path
 
 # Setup of Dbus:
@@ -38,44 +39,57 @@ logger.addHandler(log_handler)
 class Album_Art_Handler:
 
     default_icon_path_str ="/usr/share/icons/hicolor/32x32/apps/spotify.png"
-    
-    def get_image_path_str(self, spotify_path_str) :
-        # Get the hash of the image (it's name)
-        hash = spotify_path_str.split('/')[-1]
-        image_path = cache_path / hash
 
-        return_path_str = self.default_icon_path_str
+    def request_handler(self, url) :
+
+        try:            
+            response = requests.get(url, timeout=1.5)
+            #logger.info("response %s",response)
+            response.raise_for_status()            
+            
+        except HTTPError as http_err:
+            logger.error(f'HTTP error occurred: %s',http_err) 
+        except Exception as err:
+            logger.error(f'error occurred: %s',err)  
+
+        else:
+            return response
+
+    def process_image_url(self, track_url) : 
+        
+        open_url = 'https://open.spotify.com/oembed?url='+track_url        
+        response = self.request_handler(open_url)
+        track_info = json.loads(response.content)
+        thumbnail_url = track_info["thumbnail_url"]
+        
+        return thumbnail_url          
+        
+    def get_image_path_str(self, track_url, wrong_url) :
+        
+        # Get the hash of the image (its file name!)
+        hash = wrong_url.split('/')[-1]
+        image_path = cache_path / hash
+        return_path_str = None             
 
         # Get a new album art image if it does not exist
-        if not image_path.exists() :
-
+        if not image_path.exists() :   
             # Try and look it up via the hash given by spotify
-            try :
-                request = requests.get(spotify_path_str, timeout=1.5)
-                
-                # If the request is successful retrieve the image at the destination
-                if request.status_code == requests.codes.ok : 
-                    open(image_path, 'wb').write(request.content)
-                
-                    # Check if the retrieved file is indeed an image
-                    if not imghdr.what(image_path) is None :        
-                        # Set the return path string to the newly aquired image as a string
-                        return_path_str = image_path.as_posix()
-
-                    else :
-                        # If it is not an image remove the file
-                        logger.warning('Retrieved file is not a valid image file')
-                        image_path.unlink()
-
-            except requests.exceptions.Timeout:
-                logger.error('Timeout on album art retrieval request')
-            except requests.exceptions.RequestException as e:
-                logger.error(e)
-                
             
-        else:
+            art_url = self.process_image_url(track_url)            
+            art_image = self.request_handler(art_url).content
+ 
+            open(image_path, 'wb').write(art_image)            
+            # Check if the retrieved file is indeed an image
+            if not imghdr.what(image_path) is None :        
+            # Set the return path string to the newly aquired image as a string
+                return_path_str = image_path.as_posix()
+            else:
+                # If it is not an image remove the file
+               logger.warning('Retrieved file is not a valid image file')
+               image_path.unlink()
+        else :
             return_path_str = image_path.as_posix()
-
+            
         return return_path_str
 
 ###
@@ -92,7 +106,9 @@ class Notification_Handler:
 
     def __init__(self, player_object):
 
-        self.notifier_object = bus.get_object('org.freedesktop.Notifications', '/org/freedesktop/Notifications')
+        self.notifier_object = bus.get_object('org.freedesktop.Notifications',
+                '/org/freedesktop/Notifications')
+
         self.iface = dbus.Interface(self.notifier_object, 'org.freedesktop.Notifications')
         self.prob_iface = dbus.Interface(player_object, 'org.freedesktop.DBus.Properties')
 
@@ -100,32 +116,39 @@ class Notification_Handler:
 
         # Replace '&' with 'and' because of some quirk in dunsts pango engine
         body = body.replace('&', '&amp;')
-        body = '<span size="x-large">' + body + '</span>\n' +\
+        body = '<span size="large">' + body + '</span>\n' +\
             '<span size="large">(' + playback_status + ')</span>'
        
         title = title.replace('&', '&amp;')
-        title = '<span size="x-large" font_weight="bold">' + title+ '</span>'
+        # title = '<span size="large">' + title+ '</span>'
 
         # Send the notification data to the notification server:
-        self.iface.Notify(self.app_name, self.id_num_to_replace, album_art, title, body, self.actions_list, self.hint, self.delay)
+        self.iface.Notify(
+                self.app_name, 
+                self.id_num_to_replace, 
+                album_art, 
+                title, body, 
+                self.actions_list, 
+                self.hint, 
+                self.delay
+                )
 
-    def notify_songinfo(self):
+    def notify_songinfo(self):        
         # Give Spotify a bit of time to update the metadata attribute.
         time.sleep(self.update_delay)
         # Read property interface data:
         info = self.prob_iface.Get('org.mpris.MediaPlayer2.Player','Metadata')
         playback_status = self.prob_iface.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
         print(playback_status)
-
+        
         # Grab a path to the album art image via the 'AlbumArt_Handler
         art_handler = Album_Art_Handler()
-        image_path_str = art_handler.get_image_path_str(str(info['mpris:artUrl']))
-
+        
+        image_path_str = art_handler.get_image_path_str(str(info['xesam:url']),str(info['mpris:artUrl']))
+        
         # Send it to the notification method
         self.notify(str(info['xesam:artist'][0]), str(info['xesam:title']), image_path_str, playback_status)
-
     
-
 ###
 # Handles all dbus communication between the given control arguments and the media player (Spotify)
 ###
